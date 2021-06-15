234ed18c26194d0d727a85b2d818bba5724d60a4
huanwlisa <huanw@outlook.com>
2021-06-15 16:31:42 +0800

    ali update

--- ALI/main.tf
+++ ALI/main.tf
@@ -1,3 +1,28 @@
+#module "oss-backend" {
+#  source                   = "terraform-alicloud-modules/remote-backend/alicloud"
+#  create_backend_bucket    = true
+#  create_Tablestore_lock_instance = true
+#  create_Tablestore_lock_table    = true
+#  region                   = "cn-beijing"
+#  state_name               = "terraform/terraform.tfstate"
+#  encrypt_state            = true
+#}
+terraform {
+  backend "oss" {
+    bucket = "potevio"
+    prefix   = "terraform"
+    key   = "terraform.tfstate"
+    region = "cn-beijing"
+    tablestore_endpoint = "https://terraform-remote.cn-beijing.ots.aliyuncs.com"
+   # tablestore_table = "statelock"
+
+  }
+  required_providers {
+    alicloud = {
+      source = "aliyun/alicloud"
+    }
+  }
+}
 provider "alicloud" {
     
 }
@@ -9,5 +34,5 @@ resource "alicloud_vpc" "alivpc" {
 resource "alicloud_vswitch" "alivsw" {
   vpc_id            = alicloud_vpc.alivpc.id
   cidr_block        = "10.0.0.0/24"
- # availability_zone = "cn-beijing-b"
+  zone_id = "cn-beijing-b"
 }
